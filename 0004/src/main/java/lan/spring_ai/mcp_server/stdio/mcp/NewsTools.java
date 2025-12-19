cb5c656deb3b7a65d7a5feed6476144340f2f7a8
ed <barrenaedu@gmail.com>
2025-12-19 13:51:02 -0300

    Using McpTool annotation so no config needed

--- 0004/src/main/java/lan/spring_ai/mcp_server/stdio/mcp/NewsTools.java
+++ 0004/src/main/java/lan/spring_ai/mcp_server/stdio/mcp/NewsTools.java
@@ -2,8 +2,8 @@ package lan.spring_ai.mcp_server.stdio.mcp;
 
 import java.util.List;
 
-import org.springframework.ai.tool.annotation.Tool;
-import org.springframework.ai.tool.annotation.ToolParam;
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springaicommunity.mcp.annotation.McpToolParam;
 import org.springframework.stereotype.Component;
 
 import lan.spring_ai.mcp_server.stdio.domain.News;
@@ -15,8 +15,8 @@ import lombok.AllArgsConstructor;
 public class NewsTools {
 	private final NewsProvider provider;
 	
-    @Tool(name = "get-top-news-stories", description = "Get the top news stories")
-	public List<News> getTopNewsStories(@ToolParam(description = "The number of stories to retrive") int limit) {
+	@McpTool(name = "get-top-news-stories", description = "Get the top news stories")
+	public List<News> getTopNewsStories(@McpToolParam(description = "The number of stories to retrive") int limit) {
 		return provider.getTopNewsStories(limit);
 	}
 
